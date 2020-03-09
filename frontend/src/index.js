659678bcdef4429916a5c1ea4d568b8525e5fdc7
ncaudill27 <nelson.a.caudill@gmail.com>
2020-03-09 19:32:01 -0400

    Confirm front to back connection

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,1 +1,5 @@
-console.log('test')
+document.addEventListener('DOMContentLoaded', ()=> {
+    fetch('http://127.0.0.1:3000/categories')
+    .then(resp => resp.json())
+    .then(json => console.log(json))
+})
