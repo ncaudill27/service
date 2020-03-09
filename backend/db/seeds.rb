c014547e1e122c2d74805e1ac6e1d59ed134352d
ncaudill27 <nelson.a.caudill@gmail.com>
2020-03-09 19:13:41 -0400

    Add Category resource and confirm index access

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Category.delete_all
+
+Category.create(name: "Food")
+Category.create(name: "Alcohol")
