54d42ef94eae5e32ed24db4d0397a69dc9853cdd
ncaudill27 <nelson.a.caudill@gmail.com>
2020-03-12 09:30:48 -0400

    Add destroy action to ItemsController

--- backend/app/controllers/items_controller.rb
+++ backend/app/controllers/items_controller.rb
@@ -3,4 +3,9 @@ class ItemsController < ApplicationController
     items = Item.all
     render json: items, only: [:id, :name, :subcategory_id]
   end
+
+  def destroy
+    item = Item.find_by_id(params[:id])
+    render json: {item_id: item.id}
+  end
 end
