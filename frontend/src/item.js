82050a0052e9c3c9fc1e50ae2c8a4bff26aff28f
ncaudill27 <nelson.a.caudill@gmail.com>
2020-03-12 09:47:53 -0400

    Add delete button to item cards

--- frontend/src/item.js
+++ frontend/src/item.js
@@ -17,6 +17,8 @@ export default class Item {
     render() {
         this.element.setAttribute('data-item-id', this.id)
         this.element.innerHTML = `
+        <img src='/deletebutton.png' alt='Delete button'>
+        <br>
         <h4>
             ${this.name}
         </h4>
