ca316afa327af6632a5a9deae1172b960a281090
ncaudill27 <nelson.a.caudill@gmail.com>
2020-03-11 22:49:06 -0400

    Add destroy action to subcategories controller

--- backend/app/controllers/subcategories_controller.rb
+++ backend/app/controllers/subcategories_controller.rb
@@ -3,4 +3,9 @@ class SubcategoriesController < ApplicationController
     subcategories = Subcategory.all
     render json: subcategories, only: [:name, :id, :category_id]
   end
+
+  def destroy
+    subcategory = Subcategory.find_by_id(params[:id])
+    render json: {subcategory_id: subcategory.id}
+  end
 end
